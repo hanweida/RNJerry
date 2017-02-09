efc4fbf677ca2f15b2b1f3b5b93d3d03f1ac37e8
hanweida <534446340@qq.com>
2017-02-09 18:14:18 +0800

    import

--- index.android.js
+++ index.android.js
@@ -13,35 +13,12 @@ import {
   PixelRatio,
 } from 'react-native';
 
+const TextHeader=require('./Text_Header');
+
 export default class RNJerry extends Component{
   render(){
     return (
-      <View style={styles.flex}>
-       <View style={styles.container}>
-        <View style={[styles.item,styles.center]}>
-          <Text style={styles.font}>
-            酒店                      
-          </Text>
-        </View>
-        <View style={[styles.item,styles.lineLeftRight]}>
-          <View style={[styles.center, styles.flex,styles.lineCenter]}>
-            <Text style={styles.font}>海外酒店</Text>
-          </View>
-          <View style={[styles.center, styles.flex]}>
-            <Text style={styles.font}>特惠酒店</Text>
-          </View>
-        </View>
-        <View style={[styles.item]}>
-          <View style={[styles.center, styles.flex,styles.lineCenter]}>
-            <Text style={styles.font}>团购</Text>
-          </View>
-          <View style={[styles.center, styles.flex]}>
-            <Text style={styles.font}>客栈，公寓</Text>
-          </View>
-        </View>
-      </View>
-    </View>
-     
+       <TextHeader></TextHeader>
     );
   }
 }
