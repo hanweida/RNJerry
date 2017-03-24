fdd6164ab64044e378cc0bc5196412861f2f0591
hanweida <534446340@qq.com>
2017-03-24 11:57:12 +0800

    orientation

--- android/app/src/main/java/com/rnjerry/MainApplication.java
+++ android/app/src/main/java/com/rnjerry/MainApplication.java
@@ -9,9 +9,10 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import com.brentvatne.react.ReactVideoPackage;
 import java.util.Arrays;
 import java.util.List;
+import com.github.yamill.orientation.OrientationPackage; 
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -24,7 +25,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new ReactVideoPackage(),
+          new OrientationPackage()
       );
     }
   };
