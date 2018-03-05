c802dde6a5964971e80d21deab1c010d74ac1dff
hanweida <534446340@qq.com>
2018-03-05 18:07:46 +0800

    RNJerry 成功运行

--- index.android.js
+++ index.android.js
@@ -17,69 +17,11 @@ import  {
     DeviceEventEmitter
     } from 'react-native';
 
-// const TextInput_1=require('./TextInput_1');
-// const TouchAble_1=require('./Touchable_1');
-// const Picker_1=require('./Picker_1');
-// import ProgressBarAndroid_1 from './ProgressBarAndroid_1';
-// import DrawerLayoutAndroid_1 from './DrawerLayoutAndroid_1';
-// import ViewPagerAndroid_1 from './ViewPagerAndroid_1';
-// import ViewPageIndex from './zujian/ViewPageIndex';
-import ToastRNModule from './nativemodules/ToastRNModule'
 export default class RNJerry extends Component {
-
-  _click(){
-    ToastRNModule.show('-成功啦');
-  }
-
-  componentWillMount(){
-    DeviceEventEmitter.addListener('AndroidToRNMessage',this.handleAndroidMessage);
-  }
-
-  componentWillunMount(){
-     DeviceEventEmitter.remove('AndroidToRNMessage',this.handleAndroidMessage);
-  }
-
-
-  handleAndroidMessage=(msg)=>{
-
-     console.log(msg);
-  }
-
-CallAndroid_callback=()=>{
-  ToastRNModule.measureLayout(
-    (msg) =>{
-      console.log(msg);
-    },(x,y,width,heigth)=>{
-      console.log(x + '坐标,' + y + '坐标,' + width + '宽,'+heigth + '高');
-    }
-  );
-}
-
-CallAndroid_promise = () => {
-
-    ToastRNModule.rnCallNative_promise('东方耀-rn调用原生模块的方法-成功啦').then(
-      (msg) => {
-        console.log('promise成功：'+msg);
-      }
-    ).catch(
-      (err) => {
-        console.log(err);
-      }
-      );
-  }
-
   render(){
     return(
         <View style={styles.container}>
-          <TouchableOpacity
-              onPress={this._click}
-          >
-            <Text>测试</Text>
-            <Text onPress={this.CallAndroid_callback} style={{fontSize:25}}>调用原生方法_使用_回调函数</Text>
-          </TouchableOpacity>
-          <Text style={{ fontSize: 25 }} onPress={this.CallAndroid_promise} >调用原生方法_使用_Promise</Text>
-            <Text>Android原生暴露的常量-{ToastRNModule.URL}</Text>
-              <Text>作者-{ToastRNModule.author}-QQ号-{ToastRNModule.qq}</Text>
+
         </View>
     );
   }
